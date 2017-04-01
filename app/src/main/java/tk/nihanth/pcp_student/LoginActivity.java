733b137c2203283d56ac0c6c2cea6e1ece02511c
Nihanth <nihanthmbc@outlook.com>
2017-04-01 09:12:46 +0530

    Initial commit

---
+++ app/src/main/java/tk/nihanth/pcp_student/LoginActivity.java
@@ -0,0 +1,16 @@
+package tk.nihanth.pcp_student;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        
+
+    }
+}
